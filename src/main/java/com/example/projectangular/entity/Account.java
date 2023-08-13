c4bdd2d549c0eee7bdad4b907931cbf33cc3ca21
lucky2588 <duwcsthang2001@gmail.com>
2023-08-13 17:48:44 +0700

    Create Account.java

---
+++ src/main/java/com/example/projectangular/entity/Account.java
@@ -0,0 +1,11 @@
+package com.example.projectangular.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Account {
+    private String username;
+    private String password;
+}
